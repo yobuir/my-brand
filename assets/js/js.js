4c0ed86291c22e96db6a62705c051e8eeddba6fd
jobeuse <iradukundajob3@gmail.com>
2023-02-01 18:09:02 +0200

    index,login,blog,signup,openbog,model page completed

--- assets/js/js.js
+++ assets/js/js.js
@@ -45,38 +45,6 @@
  }
 
 
-    var burger_button= document.getElementById("burger_button"); 
-    var navbar= document.getElementById("NavItems_icons"); 
-    var close= document.getElementById("close"); 
-
-function showNavabar (){ 
-    burger_button.addEventListener("click", function (){
-        navbar.style.display="block";  
-        burger_button.style.display="none"
-        close.style.display="block";
-    });
-
-   
-   
-}
-
-
-function closeNavbar (){ 
-     close.style.display="none";
-
-     if(close){ 
-         close.addEventListener("click", function (){
-                navbar.style.display="none"; 
-                burger_button.style.display="block"
-                  close.style.display="none";
-             });
-    }
-
-}
-
-
-showNavabar();
-closeNavbar();
 
  
 
